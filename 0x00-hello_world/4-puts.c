ce4c4d7968b37901feec903e6d2a0f55c29c12b7
Kelvin <koolkt10@gmail.com>
2023-09-07 07:29:15 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
-
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -9,6 +8,5 @@ int main(void)
 {
 
 	puts("Programming is like building a multilingual puzzle");
-
-	Return (0);
+	return (0);
 }
