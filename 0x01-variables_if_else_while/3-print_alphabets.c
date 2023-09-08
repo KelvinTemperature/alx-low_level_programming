9742c791554309c145d4fb0b975e5e93efd3bdcf
Kelvin <koolkt10@gmail.com>
2023-09-08 16:21:24 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp;
+	char calp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		putchar(alp);
+	}
+	for (calp = 'A'; calp <= 'Z'; calp++)
+	{
+		putchar(calp);
+	}
+	putchar('\n')
+	return (0);
+}
