16d8f5189257b4451ffb0cd10b1956c8c1e03a6a
Kelvin <koolkt10@gmail.com>
2023-09-12 15:54:37 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a given number
+ *
+ * @i: the number to evaluate
+ *
+ * Return:value of last digit
+ */
+
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
