077763270f397ba3a1f07e12f3a603cf29580594
Kelvin <koolkt10@gmail.com>
2023-09-12 14:43:53 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "man.h"
+
+/**
+ * _abs - prints the absolute value of a number
+ *
+ * @n: the number to be evaluated
+ *
+ * Return: the absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		_putchar(n);
+	}
+	else
+	{
+		n = n / -1;
+		-putchar(n);
+	}
+
+}
