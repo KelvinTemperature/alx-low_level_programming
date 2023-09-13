b74b7cfdef6971a93790d74a17e6ec0a1c56c4b8
Kelvin <koolkt10@gmail.com>
2023-09-12 18:45:55 -0700

    to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,13 +18,16 @@ void print_to_98(int n)
 			printf("%d, ", n);
 		}
 	}
-	else
+	if (n < 98)
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
 	}
-	printf("%d\n", n);
+	else
+	{
+		printf("%d\n", n);
+	}
 
 }
