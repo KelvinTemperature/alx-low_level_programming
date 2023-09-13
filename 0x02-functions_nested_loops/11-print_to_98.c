7e0eb857963311fc76ae0c632bb6ed24c6ec7c51
Kelvin <koolkt10@gmail.com>
2023-09-12 17:48:23 -0700

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print integer to 98
+ *
+ * @n: is the given number
+ *
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	for (n = 0; n < 99; n++)
+	{
+		if (i != 98)
+		{
+			printf("%d, ", n);
+		}
+		else
+		{
+			printf("%d\n", n);
+		}
+	}
+
+}
