43dbc1c5a8e7793b0dd1f540b03bd1f37f08b040
Kelvin <koolkt10@gmail.com>
2023-09-13 10:06:20 -0700

    fibonacci more

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - the first function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int n1, n2, next;
+	int n;
+
+	n1 = 1;
+	n2 = 2;
+
+	printf("%ld, %ld", n1, n2);
+
+	for (n = 0; n < 96; n++)
+	{
+		next = n1 + n2;
+		printf(", %ld", next);
+		n1 = n2;
+		n2 = next;
+	}
+	printf("\n");
+
+	return (0);
+}
