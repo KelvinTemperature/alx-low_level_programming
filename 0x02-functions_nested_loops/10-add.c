16cd1c1d2ca53d7f2c54bcd9e678418fba9d8a67
Kelvin <koolkt10@gmail.com>
2023-09-12 17:15:51 -0700

    add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include main.h
+
+/**
+ * add - adds two integers
+ *
+ * @a: integer to add
+ * @b: integer to add
+ *
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
