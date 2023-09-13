8d75cbd5490abe6423ea16fcd83feb238ec5e8fa
Kelvin <koolkt10@gmail.com>
2023-09-13 10:33:32 -0700

    fibonacci more

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,12 +14,12 @@ int main(void)
 	n1 = 1;
 	n2 = 2;
 
-	printf("%lf, %lf", n1, n2);
+	printf("%f, %f", n1, n2);
 
 	for (n = 0; n < 96; n++)
 	{
 		next = n1 + n2;
-		printf(", %lf", next);
+		printf(", %f", next);
 		n1 = n2;
 		n2 = next;
 	}
