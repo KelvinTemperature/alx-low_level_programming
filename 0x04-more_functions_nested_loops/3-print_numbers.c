ede42ce8714ad2d0870d63eda74e70b679b90b2f
Kelvin <koolkt10@gmail.com>
2023-09-14 08:56:42 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#inlude "main.h"
+
+/**
+ * print_numbers - print numbers 0 - 9
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	for (int i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+
+}
