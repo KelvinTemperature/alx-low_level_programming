5bb43e403256aaa525be33087b6b720e31cec62f
Kelvin <koolkt10@gmail.com>
2023-09-14 15:50:02 -0700

    print a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ *
+ * @n: number times _ will be printed
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i = 0;
+
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
