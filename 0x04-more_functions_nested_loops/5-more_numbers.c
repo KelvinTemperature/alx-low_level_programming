8b05030a2aa127f02824df99f4fedec6ae99c33d
Kelvin <koolkt10@gmail.com>
2023-09-14 10:26:02 -0700

    more number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int m;
+
+	for (m = 1; m <= 10; m++)
+	{
+
+	for (n = 0; n <= 14; n++)
+	{
+		if (n >= 10)
+		{
+			_putchar((n / 10) + 48);
+			_putchar((n % 10) + 48);
+		}
+		_putchar(n + 48);
+
+	}
+	}
+
+}
