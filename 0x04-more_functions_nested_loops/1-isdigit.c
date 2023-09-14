ef1e6b8fb182da80792aef05885046782c39c502
Kelvin <koolkt10@gmail.com>
2023-09-14 08:38:55 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for digits
+ *
+ * @c: the variable to check
+ *
+ * Return: 1 for positive case 0 for otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
