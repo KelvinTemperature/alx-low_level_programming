fe88c585a39aff87f431add0d5216ae4aceed5dc
Kelvin <koolkt10@gmail.com>
2023-09-18 14:40:50 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print string to stdout
+ *
+ * @str: string
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(i);
+		i++;
+	}
+
+}
