f50d8d136cd8ed950ef451323fdbfaaaca65fa01
Kelvin <koolkt10@gmail.com>
2023-09-20 22:59:23 -0700

    concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,9 +16,11 @@ char *_strcat(char *dest, char *src)
 
 	for (len1 = 0; src[len1] != '\0'; len1++)
 	{
+
 	}
 	for (len2 = 0; dest[len2] != '\0'; len2++)
 	{
+
 	}
 	for (i = 0; i <= len2; i++)
 	{
