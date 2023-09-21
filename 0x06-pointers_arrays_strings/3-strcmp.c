d584e1474794887b49b9a3c546ebaa6f18752485
Kelvin <koolkt10@gmail.com>
2023-09-21 10:50:42 -0700

    compares

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * _strcmp - comapres s2 to s1
+ * _strcmp - compares two strings
  *
- * @s1: First String
- * @s2: Second String
+ * @s1: first string
+ * @s2: second string
  *
- * Return: the value of s1-s2
+ * Return: - 0 +
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
-		else if (s1[i] == s2[i])
+		else
 		{
 			return (0);
 		}
