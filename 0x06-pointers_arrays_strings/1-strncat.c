ecc88127984371f9a560a79ffc28b5534ff675f3
Kelvin <koolkt10@gmail.com>
2023-09-20 23:35:57 -0700

    cat n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (len1 = 0; dest[len1] != '\0'; len1++)
 	{
 	}
-	for (i = 0; i <= n; i++)
+	for (i = 0; src[i] != '\0' && i <= n; i++)
 	{
 		dest[len1 + i] = src[i];
 	}
