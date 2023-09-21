1b3f98986036f4fb176e3fcb7843f19e597dfb2d
Kelvin <koolkt10@gmail.com>
2023-09-21 10:14:20 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,16 +12,12 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int value;
 
-	while ((s1[i] != '\0') && (s2[i] != '\0'))
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			value = s1[i] - s2[i];
-			break;
-
-			return (value);
+			return (s1[i] - s2[i]);
 		}
 		else if (s1[i] == s2[i])
 		{
