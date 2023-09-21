a0fd5f048a73ba5ecc61b29b5666460d41935dcc
Kelvin <koolkt10@gmail.com>
2023-09-20 23:15:04 -0700

    concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,17 +14,17 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int len1, len2;
 
-	for (len1 = 0; src[len1] != '\0'; len1++)
+	for (len1 = 0; dest[len1] != '\0'; len1++)
 	{
 
 	}
-	for (len2 = 0; dest[len2] != '\0'; len2++)
+	for (len2 = 0; src[len2] != '\0'; len2++)
 	{
 
 	}
-	for (i = 0; i <= len1; i++)
+	for (i = 0; i <= len2; i++)
 	{
-		dest[len2 + i] = src[i];
+		dest[len1 + i] = src[i];
 	}
 
 	return (dest);
