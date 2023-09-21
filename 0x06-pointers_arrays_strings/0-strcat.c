d207fd003f657745e3050f6a3dd548ec40eb0f05
Kelvin <koolkt10@gmail.com>
2023-09-20 22:30:29 -0700

    concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate strings
+ *
+ * @dest: destination variable
+ * @src: source variable
+ *
+ * Return: pointer to dest variable
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	int len1, len2;
+
+	for (len1 = 0; src[i] != 0; len1++)
+	{
+	}
+	for (len2 = 0; dest[i] != 0; len2++)
+	{
+	}
+	for (i = 0; i <= len2; i++)
+	{
+		dest[len + i] = src[i];
+	}
+
+	return (dest);
+}
