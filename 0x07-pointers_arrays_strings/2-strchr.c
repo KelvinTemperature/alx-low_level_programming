58e539d4b66a33a71e12c434a58d1150cfcf5cfe
Kelvin <koolkt10@gmail.com>
2023-09-25 12:21:17 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,14 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
 
-	for (i = 0; i < strlen(s); i++)
-		if (*(s + i) == c)
+	while (*s != '\0')
+	{
+		if (*s == c)
 		{
-			return (s + i);
+			return (s);
 		}
-		else if (*(s + i) == '\0')
-			return (NULL);
+		s++;
+	}
+	return (NULL);
 }
