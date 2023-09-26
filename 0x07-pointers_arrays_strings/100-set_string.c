ee244161783cbccbb5aea6f35c4fe2431ca18135
Kelvin <koolkt10@gmail.com>
2023-09-25 19:49:02 -0700

    multi array

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - set the value of a pointer to a char
+ * @s: array of an array
+ * @to: an array
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
