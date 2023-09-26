acccf4513e609a43db08589f5737be1652cb564f
Kelvin <koolkt10@gmail.com>
2023-09-25 18:58:56 -0700

    chess board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,11 +11,9 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	int n = strlen(*a);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < 8; i++)
 	{
-		j = 0;
 		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
