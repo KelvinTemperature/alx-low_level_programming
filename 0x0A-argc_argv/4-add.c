60e2f492fa497fcd5e924cc1ea99ddfdc95559c2
Kelvin <koolkt10@gmail.com>
2023-10-02 19:55:18 -0700

    add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the entry function
+ * @argc: argument count
+ * @argv: array of argument
+ * Return: 0 for success
+ */
+
+int main(int argc, char **argv)
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+	{
+	for (i = 0; i < argc; i++)
+	{
+		if ((*argv[i] >= 58) && (*argv[i] <= 126))
+		{
+			printf("Error\n");
+
+			return (1);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+
+	return (0);
+}
