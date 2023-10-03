cfa3aabb61b46933ea999ecc34c75b0657ad4eb8
Kelvin <koolkt10@gmail.com>
2023-10-02 18:46:29 -0700

    add two number

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the entry function
+ * @argc: argument count
+ * @argv: array of argument
+ * Return: 0 for success
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		int result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+}
