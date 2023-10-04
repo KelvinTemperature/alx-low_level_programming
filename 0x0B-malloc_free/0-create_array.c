d7d0bf8341c0e614b9a5b62b7c74a60d4e7706dc
Kelvin <koolkt10@gmail.com>
2023-10-03 23:51:22 -0700

    create an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,8 +17,8 @@ char *create_array(unsigned int size, char c)
 	s = malloc(size * sizeof(char));
 	if (size == 0)
 	{
-		exit(EXIT_FAILURE);
 		return (NULL);
+		exit(EXIT_FAILURE);
 	}
 
 	for (i = 0; i < size; i++)
