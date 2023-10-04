8014fe043b42586a5f4ec58b860954143c6a5ed3
Kelvin <koolkt10@gmail.com>
2023-10-04 06:24:46 -0700

    concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,22 +13,21 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len;
 	char *str;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (i = 0; s1[i] != '\0'; i++)
 		len++;
 	for (j = 0; s2[j] != '\0'; j++)
 		len++;
 	len = len + 1;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
 	str = malloc(sizeof(char) * len);
 
 	if (str == NULL)
 	{
-		free(str);
 		return (NULL);
 	}
 	for (i = 0; s1[i] != '\0'; i++)
