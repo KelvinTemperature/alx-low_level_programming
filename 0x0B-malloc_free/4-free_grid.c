da93df02edbfcf97b4c3dc41bf82632ffb8058fe
Kelvin <koolkt10@gmail.com>
2023-10-04 12:18:45 -0700

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * fre_grid - free allocated grid bytes
+ * @grid: pointed grid
+ * @height: each array
+ * Return: Nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(arr[i]);
+
+	free(arr);
+}
