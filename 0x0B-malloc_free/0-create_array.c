0813d71a6d37bd936f78de2d0d59066c841a60ee
Kelvin <koolkt10@gmail.com>
2023-10-03 22:52:22 -0700

    create an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *s;
 
-	s = malloc((size + 1) * sizeof(char));
+	s = malloc(size * sizeof(char));
 	if (size == 0)
 	{
 		return (NULL);
@@ -24,7 +24,6 @@ char *create_array(unsigned int size, char c)
 	{
 		s[i] = c;
 	}
-	s[i] = '\0';
 
 	return (s);
 }
