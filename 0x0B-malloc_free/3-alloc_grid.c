6a62cf5572ace13ffe4a02a22e2f6a9f848a1a0c
Kelvin <koolkt10@gmail.com>
2023-10-04 16:05:24 -0700

    2-dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,8 +24,15 @@ int **alloc_grid(int width, int height)
 		arr[i] = (int *)malloc(width * sizeof(int));
 		if (arr[i] == NULL)
 		{
-			return (NULL);
-			free(arr);
+			if (arr[i] == NULL)
+			{
+				for (j = 0; j < width; j++)
+				{
+					free(arr[j]);
+				}
+				free(arr);
+				return (NULL);
+			}
 		}
 	}
 
