ba67de2c2cd384c046074dcbea49320dcef735f2
Kelvin <koolkt10@gmail.com>
2023-10-08 13:08:52 -0700

    malloc corected

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < len1; i++)
 		str[i] = s1[i];
 
 	for (j = 0; j < n; j++, i++)
