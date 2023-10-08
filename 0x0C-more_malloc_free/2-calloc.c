87320ccb77d7a2d96760d44cf3dbe575adc04125
Kelvin <koolkt10@gmail.com>
2023-10-08 11:28:54 -0700

    malloc corrections

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,10 +15,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	arr = malloc(sizeof(unsigned int) * nmemb * size);
 
 	if (arr == NULL)
 		return (NULL);
+
 	for (i = 0; i < (nmemb * size); i++)
 		arr[i] = 0;
 
