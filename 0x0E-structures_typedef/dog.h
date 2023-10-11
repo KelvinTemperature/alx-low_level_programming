2fd63eb352839297461193869b7c7e2e608ecd5a
Kelvin <koolkt10@gmail.com>
2023-10-10 18:33:58 -0700

    free dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define DOG_H
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - Houses the properties of dog
  * @name: name of the dog
  * @age: age of the dog
@@ -16,10 +21,7 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
