b80c8ba6f5ae56e271c7faf0b1489e7619366c09
Kelvin <koolkt10@gmail.com>
2023-10-10 18:45:01 -0700

    typedef new dog correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,12 +21,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (new_dog->name == NULL)
 	{
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
 	new_dog->owner = malloc(sizeof(owner));
 
 	if (new_dog->owner == NULL)
 	{
+		free(new_dog->owner);
+		free(new_dog);
 		return (NULL);
 	}
 
