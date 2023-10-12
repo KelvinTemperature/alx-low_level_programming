ac7e98a85f8268e2760858d029f6fcb1142baf30
Kelvin <koolkt10@gmail.com>
2023-10-12 16:59:45 -0700

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,11 +18,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(s, const char *);
+
 		if (str == NULL)
 			printf("(nil)");
-		printf("%s", str);
+		else
+			printf("%s", str);
+
 		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
+
+	va_end(s);
 }
