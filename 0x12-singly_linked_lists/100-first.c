a2142e6ebe50755411db4b4d3cef7d57073e96d0
Kelvin <koolkt10@gmail.com>
2023-10-19 17:37:08 -0700

    print before main handled

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,7 @@
  * @(constructor): condition to print before main
  */
 
-void print_first(void) __attribute__((constructor));
-
-void print_first(void)
+void __attribute__((constructor)) print_first()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
