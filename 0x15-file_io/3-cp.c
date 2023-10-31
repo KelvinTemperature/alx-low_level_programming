c08d6b19a1c92702d02faba890e9af0becccd5cf
Kelvin <koolkt10@gmail.com>
2023-10-30 18:12:57 -0700

    cp file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * handle_exit_with_error - handles errors and exiting codes
+ * @code: the number to exit with
+ * @message: the error message
+ * @arg1: argument vector
+ */
+
+void handle_exit_with_error(int code, const char *message, const char *arg1)
+{
+	dprintf(STDERR_FILENO, message, arg1);
+	exit(code);
+}
+
+/**
+ * main - Entry function
+ * @argc: argument count
+ * @argv: argement vector
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	char *file_from, *file_to;
+	ssize_t read_bytes, write_bytes;
+	int file_des_from, file_des_to;
+	char buffer[BUF_SIZE];
+
+	if (argc != 3)
+		handle_exit_with_error(97, "Usage: cp file_from file_to\n", argv[1]);
+	file_from = argv[1];
+	file_to = argv[2];
+
+	file_des_from = open(file_from, O_RDONLY);
+	if (file_des_from == -1)
+		handle_exit_with_error(98, "Error: Can't read from file %s\n", file_from);
+
+	file_des_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_des_to == -1)
+		handle_exit_with_error(99, "Error: Can't read from file %s\n", file_to);
+
+	while ((read_bytes = read(file_des_from, buffer, BUF_SIZE)) > 0)
+	{
+		write_bytes = write(file_des_to, buffer, read_bytes);
+		if (write_bytes != read_bytes)
+			handle_exit_with_error(99, "Error: Can't read from file %s\n", file_to);
+	}
+	if (read_bytes == -1)
+	{
+		handle_exit_with_error(98, "Error: Can't read from file %s\n", file_from);
+	}
+
+	if (close(file_des_from) == -1)
+		handle_exit_with_error(100, "Error: Can't close fd %d\n", file_from);
+
+	if (close(file_des_to) == -1)
+		handle_exit_with_error(100, "Error: Can't close fd %d\n", file_to);
+
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define BUF_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
