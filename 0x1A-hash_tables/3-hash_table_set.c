ab04b7ab51734233d275ff12c29a9d2b80fea361
Kelvin <koolkt10@gmail.com>
2023-12-22 20:26:24 -0800

    fixing code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -39,6 +39,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht->array[index] == NULL)
 		ht->array[index] = point;
+	else if (strcmp(key, tmp->key) == 0)
+	{
+		free(tmp->value);
+		tmp->value = strdup(value);
+	}
 	else
 	{
 		point->next = tmp;
